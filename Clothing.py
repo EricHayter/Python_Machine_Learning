3d829e3cc4247db8a9c39703aa1c880daec8c668
Eric <hayter.eric@gmail.com>
2022-01-22 09:45:35 -0500

    reformated coded and added comments

--- Clothing.py
+++ Clothing.py
@@ -1,3 +1,10 @@
+'''
+Eric Hayter
+Jan 22, 2022.
+
+Another simple machine learning model that can tell what type of clothing a picture is.
+'''
+
 # TensorFlow and tf.keras
 import tensorflow as tf
 
@@ -7,29 +14,30 @@ import matplotlib.pyplot as plt
 
 fashion_mnist = tf.keras.datasets.fashion_mnist
 
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+(train_images, train_labels), (test_images,
+                               test_labels) = fashion_mnist.load_data()
 
 # storing the meaning of the labels in an array
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-
+class_names = [
+    'T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt',
+    'Sneaker', 'Bag', 'Ankle boot'
+]
 
 # simplfying the luminosity of the pictures to be a value between 0 and 1
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
-
 # LOOKING AT THE DATA
 # showing the first 25 images of the dataset
 # sets the wdith of the created window
-plt.figure(figsize=(10,10))
+plt.figure(figsize=(10, 10))
 for i in range(25):
     # organizes the pictures into a 5x5 grid
-    plt.subplot(5,5,i+1)
+    plt.subplot(5, 5, i + 1)
     # removing the ticks from the chart
     plt.xticks([])
     plt.yticks([])
-    plt.grid(False)    
+    plt.grid(False)
     # makes a picture from the arrays
     # cmap will change the color of the photos to black and white instead of the default green and blue
     plt.imshow(train_images[i], cmap=plt.cm.binary)
@@ -40,7 +48,7 @@ plt.show()
 # TRAINING THE MODEL
 model = tf.keras.Sequential([
     # flattening our intial 28x28 image into a 784 character long array
-    tf.keras.layers.Flatten(input_shape=(28,28)),
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
     tf.keras.layers.Dense(128, activation='relu'),
     # finish with the final layer being a 10 since there are 10 possible outputs
     tf.keras.layers.Dense(10)
@@ -51,8 +59,7 @@ model.compile(
     optimizer='adam',
     # The loss function helps how accurate the model is during training. This will help "steer" the model in the right direction.
     loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    metrics=['accuracy']
-)
+    metrics=['accuracy'])
 
 # train our model using the training input and outputs.
 # Training is repeated 10 times per test to allow for maximum training without overfitting.
@@ -65,12 +72,12 @@ print('\nTest Accuracy:', test_acc)
 # once the model is trained you can use the model to predict individual pieces of data like this.
 # here we use the same model as a layer but also add on the softmax which tells us the machine's confidence in each
 # answer.
-probability_model = tf.keras.Sequential([model, 
-                                         tf.keras.layers.Softmax()])
+probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
 
 predictions = probability_model(test_images)
-# This will show the machine's confidence in each choice
+# This will show the machine's confidence in each choiceF
 print(predictions[0])
+
 
 # function to print out the pictures of test data
 # it will then write underneath the prediction the percent confidence
@@ -90,12 +97,13 @@ def plot_image(i, predictions_array, true_label, img):
         color = 'red'
 
     plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
-                                    100*np.max(predictions_array),
-                                    class_names[true_label]),
-                                    color=color)
+                                         100 * np.max(predictions_array),
+                                         class_names[true_label]),
+               color=color)
+
 
 # prints out a bar graph beside the picture to show the confidence in each option
-def plot_value_array(i,predictions_array, true_label):
+def plot_value_array(i, predictions_array, true_label):
     true_label = true_label[i]
     plt.grid(False)
     plt.xticks(range(10))
@@ -107,10 +115,11 @@ def plot_value_array(i,predictions_array, true_label):
     thisplot[predicted_label].set_color('red')
     thisplot[true_label].set_color('blue')
 
+
 i = 0
-plt.figure(figsize=(6,3))
-plt.subplot(1,2,1)
+plt.figure(figsize=(6, 3))
+plt.subplot(1, 2, 1)
 plot_image(i, predictions[i], test_labels, test_images)
-plt.subplot(1,2,2)
-plot_value_array(i, predictions[i],  test_labels)
+plt.subplot(1, 2, 2)
+plot_value_array(i, predictions[i], test_labels)
 plt.show()
--- flowers.py
+++ flowers.py
@@ -9,33 +9,35 @@ from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 
 import pathlib
-dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
-data_dir = tf.keras.utils.get_file('flower_photos', origin=dataset_url, untar=True)
-data_dir = pathlib.Path(data_dir)
 
-print(tf.__version__)
+dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+data_dir = tf.keras.utils.get_file('flower_photos',
+                                   origin=dataset_url,
+                                   untar=True)
+data_dir = pathlib.Path(data_dir)
 
 batch_size = 32
 img_width = 180
 img_height = 180
 
-train_ds = tf.keras.preprocessing.image_dataset_from_directory (
-  data_dir,
-  validation_split=0.2,
-  subset="training",
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset="training",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
 
 val_ds = tf.keras.preprocessing.image_dataset_from_directory(
-  data_dir,
-  validation_split=0.2,
-  subset="validation",
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
+    data_dir,
+    validation_split=0.2,
+    subset="validation",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
 
 class_names = train_ds.class_names
+print(class_names)
 
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
@@ -45,29 +47,29 @@ val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 num_classes = len(class_names)
 
 model = Sequential([
-  layers.experimental.preprocessing.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
-  layers.Conv2D(16, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(32, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(64, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Flatten(),
-  layers.Dense(128, activation='relu'),
-  layers.Dense(num_classes)
+    layers.experimental.preprocessing.Rescaling(1. / 255,
+                                                input_shape=(img_height,
+                                                             img_width, 3)),
+    layers.Conv2D(16, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(32, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(64, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(num_classes)
 ])
 
 model.compile(
-  optimizer='adam',
-  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-  metrics=['accuracy']
-)
+    optimizer='adam',
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['accuracy'])
 
-epochs=10
-history = model.fit(
-  train_ds,
-  validation_data=val_ds,
-  epochs=epochs
-)
+print("Model shape:", model.output_shape)
+model.summary()
+
+epochs = 5
+history = model.fit(train_ds, validation_data=val_ds, epochs=epochs)
 
 model.evaluate(val_ds)
